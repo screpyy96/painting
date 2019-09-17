e06e7dca09a1a803ed960b912203e7a859a5614b
Iosif Screpciuc <45086394+screpyy96@users.noreply.github.com>
2019-09-17 11:58:04 +0100

    first commit

---
+++ dist/js/index.js
@@ -0,0 +1,40 @@
+// Select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+
+// use querySelectorAll (nav-items)
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set the overlay for the menu
+
+let showMenu = false;
+
+// add event listener for menuBtn
+
+menuBtn.addEventListener("click", toggleMenu);
+
+//Create function toggleMenu and check the state of the menu
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+
+    // Set menu to true
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    // Set menu back to false
+    showMenu = false;
+  }
+}
